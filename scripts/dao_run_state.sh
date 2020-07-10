e103410f956b3f627930433cee4426ec73d4b71a
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2020-07-10 10:53:27 -0700

    s2557 - no opencadc credentials, make the script run from bucket instead.

--- scripts/dao_run_state.sh
+++ scripts/dao_run_state.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 COLLECTION="dao"
-IMAGE="opencadc/${COLLECTION}2caom2"
+IMAGE="bucket.canfar.net/${COLLECTION}2caom2"
 
 echo "Get image ${IMAGE}"
 docker pull ${IMAGE} || exit $?
+
+echo "Get a proxy certificate"
+cp $HOME/.ssl/cadcproxy.pem ./ || exit $?
 
 echo "Run image ${IMAGE}"
 docker run --rm --name ${COLLECTION}_run -v ${PWD}:/usr/src/app/ ${IMAGE} ${COLLECTION}_run_state || exit $?
